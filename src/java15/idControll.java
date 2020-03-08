a4a49bc35f7d10ca93a8791013fee88fac0dfad0
Aydogdy <ashahyrov@gmail.com>
2020-03-08 00:38:58 -0600

    java12

--- src/java15/idControll.java
+++ src/java15/idControll.java
@@ -5,19 +5,23 @@ import java.util.Scanner;
 public class idControll {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        System.out.println("enter your code: ");
-        europe(myScanner.nextInt());
+        System.out.print("Where are you going: ");
+        myMethod(myScanner.nextLine());
+
     }
-    public static void europe (int myCode){
-        if (myCode==12341){
+    public static void myMethod (String where){
+        if (where.equalsIgnoreCase("europe")){
             System.out.println("please use gata A");
         }
-        else if (myCode==12342){
+        else if (where.equalsIgnoreCase("Africa")){
             System.out.println("please use gate B");
+        }
+        else if (where.equalsIgnoreCase("Asia")){
+            System.out.println("please use gate C");
         }
         else {
             System.out.println("wrong password");
-        }
 
+        }
     }
 }
